d10a918fb8deaba7875517d836cf44aa150bd1c3
emmanuelangelo <chinonsomaduka2000@gmail.com>
2023-02-21 11:46:55 +0100

    new mode

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import './normal.css';
 
 function App() {
   return (
     <div className="App">
-      <aside classname="sidemenu">
-        <h1>aside</h1>
+      <aside className="sidemenu">
+        <div className="sidetext">
+          <span>+</span>
+          Generate New
+        </div>
       </aside>
       <section className="chatbox">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </section>
     </div>
   );
