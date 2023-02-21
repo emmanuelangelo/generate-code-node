7dc96c71d546f8220429b1f29a784fba3354bcc2
emmanuelangelo <chinonsomaduka2000@gmail.com>
2023-02-21 22:17:46 +0100

    heheheh

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import axios from "axios"
 import './App.css';
 import './normal.css';
-import {useState} from 'react';
+import { useState } from 'react';
 
 
 function App() {
 
-const [input, setInput] = useState("");
-const [messages, setMessages] = useState([]);
-// const [chatLog, setChatLog] = useState([]);
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  // const [chatLog, setChatLog] = useState([]);
 
   const handleSubmit = async (e) => {
-  e.preventDefault();
-  let cache = [...messages,{user:"me", message: input}] 
-  setMessages(cache)   
-  try{
-      const response = await axios.post('http://localhost:5000', {prompt: input })
+    e.preventDefault();
+    let cache = [...messages, { user: "me", message: input }]
+    setMessages(cache)
+    try {
+      const response = await axios.post('https://wahala.onrender.com', { prompt: input })
       setInput("")
-      setMessages([...cache,{user:"AI", message: response.data.bot}])
-      }catch(err){
-        
-      }
+      setMessages([...cache, { user: "AI", message: response.data.bot }])
+    } catch (err) {
+
+    }
   }
-  
+
   return (
     <div className="App">
       {/* <aside className="sidemenu">
@@ -32,29 +32,29 @@ const [messages, setMessages] = useState([]);
         </div>
       </aside> */}
       <section className="chatbox" rows="1">
-      {messages.map((message)=><ChatMessage message={message} />)}
-     
+        {messages.map((message) => <ChatMessage message={message} />)}
+
         <form className="chat__form" onSubmit={handleSubmit}>
-          <input rows="1" value={input}  
-          onChange={(e) => setInput (e.target.value)}
-          className="chat-input-text" placeholder="Input Syntax" />
+          <input rows="1" value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="chat-input-text" placeholder="Input Syntax" />
           <button type='submit' className='chat__button'>Generate</button>
         </form>
-      
+
       </section>
     </div>
   );
 }
-const ChatMessage = ({message}) => {
+const ChatMessage = ({ message }) => {
   return (
     <div className="chat-message">
       <div className="chat-message-center">
         <div className={`avatar ${message?.user ? "gpt" : "chatgpt"}`}>
 
         </div>
-        <div className="message">
+        <pre className="message">
           {message?.message}
-        </div>
+        </pre>
       </div>
     </div>
   )
