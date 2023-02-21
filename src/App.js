573b0e307199be1f07b7df10b8f2547833480223
emmanuelangelo <chinonsomaduka2000@gmail.com>
2023-02-21 11:03:58 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,20 @@ function App() {
         <h1>aside</h1>
       </aside>
       <section className="chatbox">
-
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </section>
     </div>
   );
