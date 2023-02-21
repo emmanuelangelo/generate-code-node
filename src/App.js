5968418f79f6c79baeab19c878ee70ebc12c2a88
emmanuelangelo <chinonsomaduka2000@gmail.com>
2023-02-21 17:05:15 +0100

    message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
 import './normal.css';
+import {useState} from 'react';
+
 
 function App() {
+
+const [input, setInput] = useState("");
+const [chatLog, setChatLog] = useState([]);
+
+  async function handleSubmit(e) {
+  e.preventDefault();
+  setChatLog([chatLog, {user: "me", message: `${input}`}])
+  setInput("");
+
+  }
   return (
     <div className="App">
       <aside className="sidemenu">
@@ -11,14 +23,29 @@ function App() {
         </div>
       </aside>
       <section className="chatbox" rows="1">
-        <div className="chat-log"></div>
+        <div className="chat-log">
+
+        </div>
         <div className="chat-message">
-          <div className="avatar"> Me </div>
-          <div className="message"> Hello World</div>
+          <div className="chat-message-center">
+            <div className="avatar"></div>
+            <div className="message">Hello World</div>
+          </div>
+        </div>
+        <div className="chat-message chatgpt">
+          <div className="chat-message-center">
+            <div className="avatar"></div>
+            <div className="message">I am AI</div>
+          </div>
         </div>
         <div className="chat-input-hold">
-          <textarea className="chat-input-text" placeholder="Input Syntax">
-          </textarea>
+          <form OnSubmit={handleSubmit}>
+            <input rows="1" 
+            value={input}  
+            onChange={(e) => setInput (e.target.value)}
+            className="chat-input-text" placeholder="Input Syntax">
+            </input>
+          </form>
         </div>
       </section>
     </div>
